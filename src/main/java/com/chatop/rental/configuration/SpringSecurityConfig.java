ff47fa21d55e667ed69fc7c631d738f2341b4ba3
Xinhe YU <aspasia.yu@gmail.com>
2024-10-22 21:44:53 +0200

    add JWT

--- src/main/java/com/chatop/rental/configuration/SpringSecurityConfig.java
+++ src/main/java/com/chatop/rental/configuration/SpringSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.chatop.rental.configuration;
 
 import com.chatop.rental.services.CustomUserDetailsService;
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,26 +10,46 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
+
+    private String jwtKey = "RgHwH7+YjFCX4nLT/PrwtutgPO43Zmtffe1yoPc/gAGN97nrVM8/eK3YUn4uvyn6";
+
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        return http.authorizeHttpRequests(auth -> {
-           auth.requestMatchers("/admin").hasRole("ADMIN");
-           auth.requestMatchers("/user").hasRole("USER");
-           auth.anyRequest().authenticated();
-        }).formLogin(Customizer.withDefaults()).build();
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/register", "/auth/login").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        SecretKeySpec secretKey = new SecretKeySpec(this.jwtKey.getBytes(), 0, this.jwtKey.getBytes().length, "HmacSHA256");
+        return NimbusJwtDecoder.withSecretKey(secretKey).macAlgorithm(MacAlgorithm.HS256).build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        return new NimbusJwtEncoder(new ImmutableSecret<>(this.jwtKey.getBytes()));
     }
 
     @Bean
--- src/main/java/com/chatop/rental/controllers/LoginController.java
+++ src/main/java/com/chatop/rental/controllers/LoginController.java
@@ -1,10 +1,25 @@
 package com.chatop.rental.controllers;
 
+import com.chatop.rental.services.JWTService;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
+
+    public JWTService jwtService;
+
+    public LoginController(JWTService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @PostMapping("auth/login")
+    public String getToken(Authentication authentication) {
+        String token = jwtService.generateToken(authentication);
+        return token;
+    }
 
     @GetMapping("/user")
     public String getUser() {
