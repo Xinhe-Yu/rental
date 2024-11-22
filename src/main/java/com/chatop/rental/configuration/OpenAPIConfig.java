6b7ed20b50445a84a564c752e52631b251d8fade
Xinhe YU <aspasia.yu@gmail.com>
2024-11-22 13:01:13 +0100

    config swagger testing with jwt

--- src/main/java/com/chatop/rental/configuration/OpenAPIConfig.java
+++ src/main/java/com/chatop/rental/configuration/OpenAPIConfig.java
@@ -1,8 +1,12 @@
 package com.chatop.rental.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +15,8 @@ public class OpenAPIConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
                 .info(new Info()
                         .title("Chatop Rental API")
                         .version("1.0")
@@ -18,5 +24,11 @@ public class OpenAPIConfig {
                         .contact(new Contact()
                                 .name("Xinhe YU")
                                 .email("xinhe.yu.dsa@gmail.com")));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
     }
 }
